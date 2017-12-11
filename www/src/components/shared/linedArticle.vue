500c91753738848a3fb3981f5f3030d9956e1fc2
nicrausaz <n.crausaz99@gmail.com>
2017-12-11 16:12:33 +0100

    lined articles

---
+++ www/src/components/shared/linedArticle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="box">
+  <article class="media">
+    <div class="media-left">
+      <figure class="image is-64x64">
+        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>{{ infos.product_name }}</strong>
+          <br>
+          {{infos.product_description}}
+        </p>
+      </div>
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <a class="level-item">
+            <span class="icon is-small"><i class="fa fa-heart"></i></span>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </article>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['infos']
+}
+</script>
--- www/src/components/views/articles.vue
+++ www/src/components/views/articles.vue
@@ -1,31 +1,64 @@
 <template>
 <div class="container">
-  <section>
+  <section class="section">
       <b-field>
-        <b-radio-button v-model="radioButton"
-            native-value="cardedView">
+        <b-radio-button v-model="selectedView" native-value="cardedView">
             <i class="fa fa-th-large"></i>
         </b-radio-button>
-        <b-radio-button v-model="radioButton"
-            native-value="listedView">
+        <b-radio-button v-model="selectedView" native-value="listedView">
             <i class="fa fa-list"></i>
         </b-radio-button>
       </b-field>
+
+      <div id="cardedArticles" v-if="isCardedView">
+        <cardedArticle v-for="article in products_list" :key="article.product_id" :infos="article"></cardedArticle>
+      </div>
+      <div id="linedArticles" v-else>
+        <lined-article v-for="article in products_list" :key="article.product_id" :infos="article"></lined-article>
+      </div>
   </section>
 </div>
 </template>
 
 <script>
+import cardedArticle from '@/components/shared/cardedArticle'
+import linedArticle from '@/components/shared/linedArticle'
 export default {
   data () {
     return {
-      radioButton: 'cardedView'
+      selectedView: 'cardedView',
+      products_list: [
+        {
+          product_id: 1,
+          product_name: 'T-Shirt baleine bleu',
+          product_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam risus mauris, placerat ut condimentum non',
+          product_price: 56.5,
+          product_brand: 'Nike',
+          product_picture: '',
+          product_category_name: 'Blouses',
+          product_category_id: 64
+        },
+        {
+          product_id: 2,
+          product_name: 'Pantalon court pour aveugle',
+          product_description: 'Un pantalon (très) minimaliste, pour aveugle',
+          product_price: 56.5,
+          product_brand: 'Nike',
+          product_picture: '',
+          product_category_name: 'Blouses',
+          product_category_id: 64
+        }
+      ]
     }
+  },
+  computed: {
+    isCardedView () {
+      return this.selectedView === 'cardedView'
+    }
+  },
+  components: {
+    cardedArticle,
+    linedArticle
   }
 }
 </script>
-
-
-<style>
-
-</style>
