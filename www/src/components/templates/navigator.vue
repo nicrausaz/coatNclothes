d95b564027cb5f88762e03653186ce76b96c583b
nicrausaz <n.crausaz99@gmail.com>
2017-12-12 15:38:19 +0100

    mobile navbar working

--- www/src/components/templates/navigator.vue
+++ www/src/components/templates/navigator.vue
@@ -1,37 +1,37 @@
 <template>
   <nav class="navbar is-primary is-fixed-top">
-  <div class="navbar-brand">
-     <router-link to="/" class="navbar-item"><img src="https://bulma.io/images/bulma-logo.png" alt="" width="112" height="28"></router-link>
-    <div class="navbar-burger burger" :class="{'is-active': mobileMenuActive}" @click="mobileMenuActive = !mobileMenuActive" data-target="">
-      <span></span>
-      <span></span>
-      <span></span>
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"><img src="https://bulma.io/images/bulma-logo.png" alt="" width="112" height="28"></router-link>
+      <button class="button navbar-burger is-primary"  @click="createMobileMenu" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button >
     </div>
-  </div>
 
-  <div class="navbar-menu">
-    <div class="navbar-start">
-      <router-link to="/" class="navbar-item">Acceuil</router-link>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          Catégories
-        </a>
-        <div class="navbar-dropdown is-boxed">
-          <router-link to="/category/all" class="navbar-item">Toutes</router-link>
-          <hr class="navbar-divider">
-          <router-link v-for="category in categories" :key="category.id" to="/category/1" class="navbar-item">{{category.label}}</router-link>
+    <div class="navbar-menu" id="navMenu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Acceuil</router-link>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Catégories
+          </a>
+          <div class="navbar-dropdown is-boxed">
+            <router-link to="/category/all" class="navbar-item">Toutes</router-link>
+            <hr class="navbar-divider">
+            <router-link v-for="category in categories" :key="category.id" to="/category/1" class="navbar-item">{{category.label}}</router-link>
+          </div>
         </div>
+        <router-link to="/articles" class="navbar-item">Articles</router-link>
       </div>
-      <router-link to="/articles" class="navbar-item">Articles</router-link>
-    </div>
 
-    <div class="navbar-end">
-      <router-link to="/wishlists" class="navbar-item" ><b-icon pack="fa" icon="heart" size="is-medium"></b-icon></router-link>
-      <router-link to="/shopbag" class="navbar-item" ><b-icon pack="fa" icon="shopping-bag" size="is-medium"></b-icon></router-link>
-      <router-link to="/user" class="navbar-item" ><b-icon pack="fa" icon="user" size="is-medium"></b-icon></router-link>
+      <div class="navbar-end">
+        <router-link to="/wishlists" class="navbar-item" ><b-icon pack="fa" icon="heart" size="is-medium"></b-icon></router-link>
+        <router-link to="/shopbag" class="navbar-item" ><b-icon pack="fa" icon="shopping-bag" size="is-medium"></b-icon></router-link>
+        <router-link to="/user" class="navbar-item" ><b-icon pack="fa" icon="user" size="is-medium"></b-icon></router-link>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
@@ -45,6 +45,19 @@ export default {
         { id: 2, name: 'jeans', label: 'Jeans' },
         { id: 3, name: 'vestes', label: 'Vestes' }
       ]
+    }
+  },
+  methods: {
+    createMobileMenu () {
+      let navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+      if (navbarBurgers.length > 0) {
+        navbarBurgers.forEach(function (el) {
+          let target = el.dataset.target
+          let $target = document.getElementById(target)
+          el.classList.toggle('is-active')
+          $target.classList.toggle('is-active')
+        })
+      }
     }
   }
 }
