d525ebd9e9620b2fe78537a8d6eea0bd6616e78e
nicrausaz <n.crausaz99@gmail.com>
2017-12-15 14:01:56 +0100

    images articles

--- www/src/components/views/articles.vue
+++ www/src/components/views/articles.vue
@@ -46,7 +46,7 @@ export default {
           product_id: 2,
           product_name: 'Pantalon court pour aveugle',
           product_description: 'Un pantalon (très) minimaliste, pour aveugle',
-          product_price: 56.5,
+          product_price: 56,
           product_brand: 'Nike',
           product_picture: '',
           product_category_name: 'Blouses',
@@ -63,24 +63,24 @@ export default {
           product_category_id: 23
         },
         {
-          product_id: 2,
-          product_name: 'Pantalon court pour aveugle',
-          product_description: 'Un pantalon (très) minimaliste, pour aveugle',
-          product_price: 56.5,
-          product_brand: 'Nike',
-          product_picture: '',
-          product_category_name: 'Blouses',
-          product_category_id: 64
+          product_id: 4,
+          product_name: 'Bonnet barbe',
+          product_description: 'Un bonnet permettant (enfin) à tout le monde d\'avoir une barbe',
+          product_price: 34,
+          product_brand: '',
+          product_picture: '/static/articles/bonnetbarbe.jpg',
+          product_category_name: 'Bonnets',
+          product_category_id: 12
         },
         {
-          product_id: 1,
-          product_name: 'T-Shirt baleine bleu',
-          product_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam risus mauris, placerat ut condimentum non',
-          product_price: 56.5,
-          product_brand: 'Nike',
-          product_picture: '',
-          product_category_name: 'Blouses',
-          product_category_id: 64
+          product_id: 5,
+          product_name: 'Pull tricoté serpent',
+          product_description: 'Augmenter vos chance de ne pas conclure ce soir !',
+          product_price: 45,
+          product_brand: '',
+          product_picture: '/static/articles/pullserpent.jpg',
+          product_category_name: 'Pulls',
+          product_category_id: 2
         },
         {
           product_id: 2,
