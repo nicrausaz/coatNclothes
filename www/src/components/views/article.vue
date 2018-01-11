4a738100c6441ae94711d473ad77c351cb8468e2
nicrausaz <n.crausaz99@gmail.com>
2018-01-11 11:00:16 +0100

    select size

--- www/src/components/views/article.vue
+++ www/src/components/views/article.vue
@@ -11,10 +11,16 @@
         <div class="tile is-parent is-vertical">
           <article class="tile is-child is-vertical">
             <p class="title">Article descr</p>
+             <b-dropdown>
+              <button class="button is-primary" slot="trigger">
+                  <span>{{ textSize }}</span>
+                  <b-icon icon="angle-down"></b-icon>
+              </button>
+              <b-dropdown-item v-for="size in availableSizes" :key="size" @click="setSize(size)">{{ size }}</b-dropdown-item>
+            </b-dropdown>
           </article>
         </div>
-        
-</div>
+      </div>
     </section>
     </div>
 </template>
@@ -25,7 +31,19 @@ import subtitle from '@/components/templates/subtitle'
 export default {
   data () {
     return {
-      articleId: this.$route.params.id
+      articleId: this.$route.params.id,
+      availableSizes: ['S', 'M', 'L', 'XL'],
+      selectedSize: ''
+    }
+  },
+  methods: {
+    setSize (size) {
+      this.selectedSize = size
+    }
+  },
+  computed: {
+    textSize () {
+      return this.selectedSize === '' ? 'Choisir la taille' : this.selectedSize
     }
   },
   created () {
