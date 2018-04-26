e5165635d9e39552286dee7ba6c9c9989fa91843
nicrausaz <n.crausaz99@gmail.com>
2018-04-26 10:39:01 +0200

    fix patch, delete adress

--- shop/src/components/shared/user/editUserAdressModal.vue
+++ shop/src/components/shared/user/editUserAdressModal.vue
@@ -14,9 +14,15 @@
         <b-input v-model="newData.adresses_locality"></b-input>
       </b-field>
       <b-field>
-        <b-checkbox v-model="newData.adresses_main" size="is-small" true-value="1" false-value="0">
+        <b-checkbox v-model="newData.adresses_main" size="is-small" true-value="1" false-value="0" :disabled="isMain">
           Adresse par défaut ?
         </b-checkbox>
+        <div style="float: right;">
+          <a class="button is-danger is-outlined" @click.stop="deleteAdress()">
+            <span>Supprimer</span>
+            <b-icon icon="trash" size="is-small"></b-icon>
+          </a>
+        </div>
       </b-field>
     </section>
     <footer class="modal-card-foot">
@@ -37,18 +43,48 @@ export default {
   created () {
     this.newData = this.adress
   },
+  computed: {
+    isMain () {
+      return this.newData.adresses_main === 1
+    }
+  },
   methods: {
+    getData () {
+      let data = {
+        adresses_locality: this.newData.adresses_locality,
+        adresses_npa: this.newData.adresses_npa,
+        adresses_street: this.newData.adresses_street
+      }
+      if (this.newData.adresses_main) {
+        data.adresses_main = 1
+      }
+      return data
+    },
     updateAdress () {
       this.axios({
         method: 'patch',
-        url: '/user/' + this.$store.state.user.users_id + '/adresses',
-        data: {
-          adresses_id: this.newData.adresses_id,
-          adresses_locality: this.newData.adresses_locality,
-          adresses_npa: this.newData.adresses_npa,
-          adresses_street: this.newData.adresses_street,
-          adresses_main: this.newData.adresses_main
-        }
+        url: '/user/' + this.$store.state.user.users_id + '/adresse/' + this.newData.adresses_id,
+        data: this.getData()
+      })
+      .then(response => {
+        this.$toast.open({
+          message: response.data.message,
+          type: 'is-success'
+        })
+        this.$emit('update')
+        this.$parent.close()
+      })
+      .catch(err => {
+        this.$toast.open({
+          message: err.response.data.message,
+          type: 'is-danger'
+        })
+      })
+    },
+    deleteAdress () {
+      this.axios({
+        method: 'delete',
+        url: '/user/' + this.$store.state.user.users_id + '/adresse/' + this.newData.adresses_id
       })
       .then(response => {
         this.$toast.open({
