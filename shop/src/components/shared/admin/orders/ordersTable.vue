419bb51a6492774d635b21952979d2aca3d4b9a3
nicrausaz <n.crausaz99@gmail.com>
2018-05-09 13:30:49 +0200

    orders admin

---
+++ shop/src/components/shared/admin/orders/ordersTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <section>
+    <h2 class="subtitle">
+      Éditer les commandes
+    </h2>
+    <hr>
+    <b-field grouped group-multiline>
+      <b-select v-model="filter.perPage">
+        <option value="5">5 par page</option>
+        <option value="10">10 par page</option>
+        <option value="20">20 par page</option>
+      </b-select>
+      <b-input placeholder="Rechercher..." type="search" icon-pack="fas" icon="search" v-model="searchContent"></b-input>
+    </b-field>
+    <b-table :data="orders" :per-page="filter.perPage" default-sort="orders_id" :mobile-cards="false">
+      <template slot-scope="props">
+        <b-table-column field="orders_id" label="No" width="40" sortable numeric>
+          {{ props.row.orders_id }}
+        </b-table-column>
+
+        <b-table-column field="orders_paid" label="Payé" sortable>
+          {{ props.row.orders_paid }}
+        </b-table-column>
+
+        <b-table-column field="orders_paidDate" label="Date de paiement" sortable>
+          {{ props.row.orders_paidDate }}
+        </b-table-column>
+
+        <b-table-column field="orders_createdDate" label="Date de commande" sortable>
+          {{ props.row.orders_createdDate }}
+        </b-table-column>
+        <b-table-column field="ordersStatus_name" label="Status" sortable>
+          {{ props.row.ordersStatus_name }}
+        </b-table-column>
+
+        <b-table-column label="Actions" centered>
+          <b-tooltip label="Détails et édition" position="is-left">
+            <a @click="showOrder(props.row.orders_id)">
+              <span class="icon is-small">
+                <b-icon icon="edit"></b-icon>
+              </span>
+            </a>
+          </b-tooltip>
+        </b-table-column>
+      </template>
+    </b-table>
+    <!-- <b-modal :active.sync="isEditing" has-modal-card>
+      <productEditModal @update="getProducts" :id="productId"></productEditModal>
+    </b-modal>
+    <b-modal :active.sync="isCreating" has-modal-card>
+      <newProductModal @create="getProducts"></newProductModal>
+    </b-modal> -->
+  </section>
+</template>
+
+<script>
+// import productEditModal from '@/components/shared/admin/products/editProductModal'
+// import newProductModal from '@/components/shared/admin/products/newProductModal'
+
+export default {
+  data () {
+    return {
+      filter: {
+        perPage: 20
+      },
+      isEditing: false,
+      isCreating: false,
+      searchContent: '',
+      orders: [],
+      orderId: 0
+    }
+  },
+  created () {
+    this.getOrders()
+  },
+  methods: {
+    getOrders () {
+      this.axios({
+        method: 'get',
+        url: '/admin/orders'
+      })
+      .then(response => {
+        this.orders = response.data
+      })
+    },
+    showOrder () {},
+    editOrder () {}
+  }
+}
+</script>
--- shop/src/router/routes.js
+++ shop/src/router/routes.js
@@ -11,7 +11,7 @@ import admin from '@/components/views/admin/admin'
 import adminProducts from '@/components/shared/admin/products/productsTable'
 import adminDashboard from '@/components/shared/admin/dashboard'
 import adminUsers from '@/components/shared/admin/users/users'
-import adminOrders from '@/components/shared/admin/orders/orders'
+import adminOrders from '@/components/shared/admin/orders/ordersTable'
 
 import NotFound from '@/components/views/404'
 
