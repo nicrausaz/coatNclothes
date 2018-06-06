8fc188cdcbee189e0c5bd285dabd8129e388c9ad
nicrausaz <n.crausaz99@gmail.com>
2018-06-06 22:00:38 +0200

    collpase categories

--- shop/src/components/shared/products/categoryNode.vue
+++ shop/src/components/shared/products/categoryNode.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul>
-    <li>
-      <router-link @click="isOpen = !isOpen" :class="{'is-active': isSelected(children.id)}" :to="/category/ + children.id">{{children.name}}</router-link>
+  <ul @click.stop="isOpen = !isOpen">
+    <router-link :class="{'is-active': isSelected(children.id)}" :to="/category/ + children.id"><b-icon :icon="getIcon()" size="is-small" v-if="children.children"></b-icon>{{children.name}}</router-link>
+    <li v-if="isOpen">
       <categoryNode v-for="child in children.children" :key="child.id" :children="child"></categoryNode>
     </li>
   </ul>
@@ -19,6 +19,9 @@ export default {
   methods: {
     isSelected (id) {
       return parseInt(this.$route.params.id) === id
+    },
+    getIcon () {
+      return this.isOpen ? 'angle-down' : 'angle-right'
     }
   }
 }
