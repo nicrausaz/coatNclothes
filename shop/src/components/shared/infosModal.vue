63a474430da6bbe223c067079612df8a62cefd4b
nicrausaz <n.crausaz99@gmail.com>
2018-06-08 13:59:38 +0200

    footer for future usage

---
+++ shop/src/components/shared/infosModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal-card" @keyup.enter="create" color>
+    <header class="modal-card-head">
+      <p class="modal-card-title">Infos</p>
+    </header>
+    <section class="modal-card-body">
+      <p>Ce site est un magasin fictif dans le cadre d'un projet d'étude.</p>
+      <p>Aucun produit / commande n'est officiel et ne sera traité.</p>
+      <p>Ce projet à été réalisé pour le module ICT 120-151.</p>
+      <p>Nicolas Crausaz & Mathias Brugger</p>
+    </section>
+    <!-- <footer class="modal-card-foot">
+      <button class="button" type="button" @click="this.$parent.close">{{$store.state.interface.cancel}}</button>
+      <button class="button is-primary" @click="create">{{$store.state.interface.confirm}}</button>
+    </footer> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
--- shop/src/components/templates/footbar.vue
+++ shop/src/components/templates/footbar.vue
@@ -1,13 +1,44 @@
 <template>
-  <nav class="navbar is-bottom is-primary">
-    <div class="navbar-item">&copy; 2017 - Nicolas Crausaz & Mathias Brügger</div>
+<div>
+  <nav class="navbar is-bottom is-primary is-fixed-bottom">
+    <div class="navbar-item items">&copy; 2018 - Nicolas Crausaz & Mathias Brügger (EPSIC ICT 120-155)</div>
     <div class="navbar-end">
       <div class="field is-grouped">
-        <a class="navbar-item" href="https://github.com/nicrausaz/coatNclothes"><b-icon icon="facebook" size="is-small"></b-icon></a>
-        <a class="navbar-item" href="https://github.com/nicrausaz/coatNclothes"><b-icon icon="twitter" size="is-small"></b-icon></a>
-        <a class="navbar-item" href="https://github.com/nicrausaz/coatNclothes"><b-icon icon="instagram" size="is-small"></b-icon></a>
-        <a class="navbar-item" href="https://github.com/nicrausaz/coatNclothes"><b-icon icon="github" size="is-small"></b-icon></a>
+        <a class="navbar-item items" @click="contactOpen = true"><b-icon icon="comment"></b-icon></a>
+        <a class="navbar-item items" @click="infoOpen = true"><b-icon icon="info"></b-icon></a>
+        <a class="navbar-item items" href="https://github.com/nicrausaz/coatNclothes"><b-icon icon="code-branch"></b-icon></a>
       </div>
     </div>
   </nav>
+    <b-modal :active.sync="infoOpen" has-modal-card>
+      <infosModal></infosModal>
+    </b-modal>
+  </div>
 </template>
+
+<script>
+import infosModal from '@/components/shared/infosModal'
+export default {
+  data () {
+    return {
+      infoOpen: false,
+      contactOpen: false
+    }
+  },
+  components: {
+    infosModal
+  }
+}
+</script>
+
+
+<style scoped>
+.items {
+  color: white;
+}
+.items:hover {
+  color: white;
+  background-color: #c20d5d;
+}
+</style>
+
